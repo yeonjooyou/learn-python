bd4b9387b39e3ba385de2d67482249966ed5b7d3
yeonjooyou <yeonjooyou@naver.com>
2021-07-18 13:35:10 +0900

    09_tupleTest

---
+++ python-test/09_tupleTest/tupleTest4.py
@@ -0,0 +1,16 @@
+import time
+def gettime():
+    now = time.localtime()
+    print(now, type(now))
+    return now.tm_hour, now.tm_min
+
+result = gettime()
+print("지금은 %d시 %d분입니다" % (result[0], result[1]))
+
+hour, minute = gettime()
+print("지금은 %d시 %d분입니다" % (hour, minute))
+
+# =============== divmod  ===============
+d, m = divmod(7, 3)
+print("몫", d)
+print("나머지", m)
